a6e919455738e2458598bebcc5d36e2ce16de982
shejing <2016192403@qq.com>
2020-09-24 23:59:06 +0800

    do a null string test

---
+++ book/corejava/ch3/Test1.java
@@ -0,0 +1,9 @@
+public class Test1
+{
+    public static void main(String[] args)
+    {
+        String test = null;
+        //it's a fault expression, null string havent member func
+        //System.out.print(test.length());
+    }
+}
