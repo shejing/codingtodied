ec7f4ddbe940543f782947aa7b4f6a2d53a53c91
shejing <2016192403@qq.com>
2020-09-27 23:59:40 +0800

    File

---
+++ book/corejava/ch3/FileTest.java
@@ -0,0 +1,9 @@
+import java.utils.*;
+
+public class FileTest
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(Paths.get("../data/myfile.txt"), "UTF-8");
+    }
+}
---
+++ book/corejava/ch3/TimeTest.java
@@ -0,0 +1,9 @@
+import java.time; 
+
+public class TimeTest
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("%tc", new Date());
+    }
+}
